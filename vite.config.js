04f3b6a6dbb212a4866b712c4928a0efc25e4b18
PreslavaObretenova <st175849@stud.uni-stuttgart.de>
2024-02-14 23:07:59 +0100

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
+
+
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/creatingspaces/"
-})
+  base: "/creatingspaces/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'Landingpage/Landingpage.html'),
+        app: resolve(__dirname, 'index.html'),
+        howto: resolve(__dirname, 'Howtouse/Howtouse.html'),
+        info: resolve(__dirname, 'Information/Information.html'),
+        aboutus: resolve(__dirname, 'About us/Aboutus.html'),
+        
+      },
+    },
+  },
+});
+
