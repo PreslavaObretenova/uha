1bd3999ac18c4b782fc844cc41144c2ea197527f
PreslavaObretenova <st175849@stud.uni-stuttgart.de>
2024-02-14 19:48:59 +0100

    second commit

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'Webapp/final_project/main.js', // Specify the entry point of your application
+  output: {
+    file: 'dist/bundle.js', // Specify the output file where Rollup will bundle your code
+    format: 'umd', // Specify the module format (e.g., UMD, CommonJS, ES module)
+    name: 'MyBundle' // Specify the name of your bundle (only required for UMD format)
+  },
+  plugins: [
+    nodeResolve(), // Resolve node modules
+    commonjs() // Convert CommonJS modules to ES modules
+  ]
+};
